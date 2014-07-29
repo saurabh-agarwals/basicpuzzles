cb1b2aeeb5421c8274130dbb57e2ddbd296ac4b3
Saurabh Agarwal <saurabh@snapstick.com>
2014-07-29 18:12:34 +0530

    Strang all countries do have players who share there birthday with other teams

--- programming/birthdayParadox.py
+++ programming/birthdayParadox.py
@@ -1,6 +1,12 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
 import re
+from datetime import datetime
+
+'''
+we need to find out find how many teams out of the 32 have at least 2 
+players who share the same birthday. 
+'''
 
 template = "{0:10} {1:20} {2:30} {3:40}"
 data = open("fifa.html",'r').read()
@@ -57,8 +63,6 @@ def createPlayersInfoStructure(playersList):
 		if len(players[info]) >= 2:
 			print template.format("BIRTHDAY" ,"NAME", "CLUB" , "COUNTRY")
 			print ("*************************************************")
-			#Put logic of sharing birthday with two differnt countries
-			#print players[info]
 			for player in players[info]:
 				print template.format(player['bday'].encode('utf-8').strip(), player['name'].encode('utf-8').strip(), 
 					player['club'].encode('utf-8').strip(), player['country'].encode('utf-8').strip())
@@ -72,6 +76,57 @@ def getAllCountryName():
 		countries.append(country.getText())
 	return countries
 
-#setGlobalVaribales()
-playersList = getPlayersList()
-createPlayersInfoStructure(playersList)
+def getPlayersListByCountry():
+	playersList = []
+	teams = getTeamsByCountry()
+	print len(teams)
+	for team in teams:
+		initTeam = {team['country']:[]}
+		#initTeam = [{team['country']:{[]}]
+		for row in team['playersTable'].findAll("tr"):
+			cells = row.findAll("td")
+			if len(cells) > 0:
+				name = cells[2].find(text=True)
+				bday = cells[3].find("span", { "class" : "bday" }).find(text=True)
+				bday = datetime.strptime(bday, '%Y-%m-%d').strftime('%-d %B')
+				club = cells[5].findChildren()[3].find(text=True)
+				#eachProcess = {'name':name, 'bday':bday,'club':club}
+				#eachProcess = {'bday':bday}
+				eachProcess = bday
+				initTeam[team['country']].append(eachProcess)
+		playersList.append(initTeam)
+	return playersList
+
+def findCommandBithday():	
+	newList = {}
+	birthDayList = []
+	birthDayListByCountry = getPlayersListByCountry()
+	count = len(birthDayListByCountry)
+	for index, playersByCountry in enumerate(birthDayListByCountry):
+		for jindex, element in enumerate(birthDayListByCountry):
+			if index == jindex:
+				continue
+			birthDayList1 = set(birthDayListByCountry[index].values()[0])
+			birthDayList2 = set(birthDayListByCountry[jindex].values()[0])
+			commanBithDay = birthDayList1.intersection(birthDayList2)				
+			commanBithDay = list(commanBithDay)
+			if commanBithDay:	
+				process = birthDayListByCountry[index].keys()[0]
+				if newList.has_key(process):
+					newList[process].append({birthDayListByCountry[jindex].keys()[0]:commanBithDay})
+				else:
+					newList[process] = [{birthDayListByCountry[jindex].keys()[0]:commanBithDay}]
+	for key in newList:
+		if len(newList[key]) > 2:
+			print key + " : " + "True"
+		#print key, newList[key], len(newList[key])
+		'''
+		print "********************"
+		print key, newList[key], len(newList[key]) 
+		print "********************"
+		'''
+	return newList
+
+findCommandBithday()
+
+
